2acf0e08ca1964414b70933f44d516d5e66b5d0c
0xtbug <tubagus.nabil19@gmail.com>
2023-06-11 12:08:53 +0700

    fix typo

--- simplied.js
+++ simplied.js
@@ -1,6 +1,6 @@
 const qrcode = require('qrcode-terminal');
 const { Client } = require('whatsapp-web.js');
-const { generateResponse, summarizeText } = require('./gpt');
+const { generateResponse, summarizeText } = require('./simplied-gpt.js');
 const { path: ffmpegPath } = require('@ffmpeg-installer/ffmpeg');
 const dotenv = require('dotenv');
 
