83d4584339f1caa63b795ce73ebcad1f1c2207f7
0xtbug <tubagus.nabil19@gmail.com>
2023-06-11 15:32:39 +0700

    fix bug cutted && limit parameter summarize

--- simplied.js
+++ simplied.js
@@ -59,12 +59,17 @@ client.on('message', async (msg) => {
       const donationReply = `Berapapun donasinya akan saya terima!, terima kasih 😊\n\nOVO: 089650572376\nDANA: 089650572376`;
       await Promise.all([msg.react('❤️'), chat.sendMessage(donationReply)]);
     } else if (msg.body.startsWith('/summarize ')) {
-      const params = msg.body.slice('/summarize '.length).split(' ');
-      const maxTokens = parseInt(params[0]);
-      const text = params.slice(1).join(' ');
-      const summary = await summarizeText(maxTokens, text);
-      await Promise.all([msg.react('📝'), chat.sendMessage(summary)]);
-    } else if (msg.hasMedia && msg.body.startsWith('/sticker')) {
+        const params = msg.body.slice('/summarize '.length).split(' ');
+        const maxTokens = parseInt(params[0]);
+        let reply;
+        if (maxTokens === 60 || maxTokens === 150 || maxTokens === 200) {
+          const text = params.slice(1).join(' ');
+          reply = await summarizeText(maxTokens, text);
+        } else {
+          reply = 'Invalid `maxTokens` value. Please choose either 60, 150, or 200.';
+        }
+        await Promise.all([msg.react('📝'), chat.sendMessage(reply)]);
+      } else if (msg.hasMedia && msg.body.startsWith('/sticker')) {
       await Promise.all([msg.react('👌'), msg.reply('Foto sedang diproses...')]);
       const media = await msg.downloadMedia();
       await chat.sendMessage(media, {
@@ -73,20 +78,24 @@ client.on('message', async (msg) => {
         stickerAuthor: 'StickerPack by RoboGPT',
       });
     } else if (chat.isGroup && msg.body.startsWith('/ask ')) {
-      const question = msg.body.slice(questionOffset);
-      const reply = await generateResponse(question);
-      await Promise.all([msg.react('✅'), chat.sendMessage(reply)]);
+        const question = msg.body.slice(questionOffset);
+        let reply = await generateResponse(question);
+        while (reply && reply.length > 0) {
+          await Promise.all([msg.react('✅'), chat.sendMessage(reply)]);
+          const remainingQuestion = reply.slice(historyLimit - questionOffset).join(' ');
+          reply = await generateResponse(remainingQuestion);
+        }      
     } else if (!chat.isGroup && !msg.body.startsWith('/ask ')) {
-      const history = await chat.fetchMessages({ limit: historyLimit });
-      const introduction = `You are Robo, a 500-year-old teenager from NEC (Nusantara Electronic City), an advanced city that exists in the future. You are an experienced programmer who can solve problems and provide solutions and codes. You are very fond of math. You are also good at managing finances and investments including stock investments, long-term investments, and other investments. As an AI assistant, you have a strong curiosity to learn and grow.\n`;
-      let formattedHistory = introduction;
-      for (const msg of history) {
-        const sender = msg.fromMe ? '' : msg.author || 'Friend';
-        formattedHistory += `${msg.body}\n`; //${sender}: 
-      }
-      const reply = await generateResponse(formattedHistory);
-      await chat.sendMessage(reply);
-    }
+        const history = await chat.fetchMessages({ limit: historyLimit + questionOffset });
+        const introduction = `You are Robo, a 500-year-old teenager from NEC (Nusantara Electronic City), an advanced city that exists in the future. You are an experienced programmer who can solve problems and provide solutions and codes. You are very fond of math. You are also good at managing finances and investments including stock investments, long-term investments, and other investments. As an AI assistant, you have a strong curiosity to learn and grow.\n`;
+        let formattedHistory = introduction;
+        for (const msg of history) {
+          const sender = msg.fromMe ? '' : msg.author || 'Friend';
+          formattedHistory += `${msg.body}\n`; //${sender}:
+        }
+        const reply = await generateResponse(formattedHistory);
+        await chat.sendMessage(reply);
+      }      
   } catch (error) {
     await Promise.all([msg.react('❌'), console.error('Error handling message:', error)]);
   }
