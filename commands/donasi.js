06c13172762178a83c11c0d3bdf6978409544dd5
0xtbug <tubagus.nabil19@gmail.com>
2023-06-12 12:56:47 +0700

    restore to default

--- commands/donasi.js
+++
@@ -1,8 +0,0 @@
-async function execute(msg, chat) {
-    const donationReply = `Berapapun donasinya akan saya terima!, terima kasih 😊\n\nOVO: 089650572376\nDANA: 089650572376`;
-    await Promise.all([msg.react('❤️'), chat.sendMessage(donationReply)]);
-  }
-  
-  module.exports = {
-    execute,
-  };  
