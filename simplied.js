60e939b5f1f3380ef29b387197a2978b721227c6
0xtbug <tubagus.nabil19@gmail.com>
2023-06-12 11:13:26 +0700

    fix error handle

--- simplied.js
+++ simplied.js
@@ -127,9 +127,11 @@ client.on('message', async (msg) => {
       await Promise.all([msg.react('✅'), chat.sendMessage(reply)]);
     } 
     // private chat
-    else if (isPrivateChat && (msg.body === '/ask' || msg.body.startsWith('/ask '))) {
-      await Promise.all([msg.react('❌'), chat.sendMessage('Command /ask hanya dapat digunakan dalam grup.')]);
-
+    else if (isPrivateChat) {
+        if (msg.body === '/ask' || msg.body.startsWith('/ask ') {
+            await Promise.all([msg.react('❌'), chat.sendMessage('Command /ask hanya dapat digunakan dalam grup.')]);
+            return;   
+        }
       const history = await chat.fetchMessages({ limit: historyLimit });
       const introduction = `You are Robo, a 500-year-old teenager from NEC (Nusantara Electronic City), an advanced city that exists in the future. You are an experienced programmer who can solve problems and provide solutions and codes. You are very fond of math. You are also good at managing finances and investments including stock investments, long-term investments, and other investments. As an AI assistant, you have a strong curiosity to learn and grow.\n`;
       let formattedHistory = introduction;
