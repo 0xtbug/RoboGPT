2f752598b6bd8bf96b7cfd4a70522292859551f8
0xtbug <tubagus.nabil19@gmail.com>
2023-06-13 02:01:35 +0700

    optimize code

--- function.js
+++ function.js
@@ -14,86 +14,75 @@ const openai = new OpenAIApi(configuration);
 // Function to generate a response based on chat history
 const generateResponse = async (chatHistory) => {
   try {
-    // Use the OpenAI API to create a completion based on the chat history
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: chatHistory,
-      max_tokens: 300, // This can be changed depending on how long you want the response to be
+      max_tokens: 300,
     });
 
-    // Remove 'Robo: ' from the start of the response, if it exists
     const response = completion.data.choices[0].text.trim().replace(/^Robo:\s*/, '');
 
     return response;
   } catch (error) {
-    // Catch specific errors if possible
-    if (error.response && error.response.status) {
-      console.log(`Error ${error.response.status}: ${error.response.data.error.message}`);
-    } else {
-      console.log(`Error: ${error.message}`);
-    }
+    handleError(error);
   }
 };
 
-// dall-e
+// Function to generate an image using DALL·E
 const drawGpt = async (text) => {
   try {
     const image = await openai.createImage({
       prompt: text,
       n: 1,
       size: "512x512"
-  });
-  const imgUrl = image.data.data[0].url;
+    });
+    const imgUrl = image.data.data[0].url;
 
-  return imgUrl;
+    return imgUrl;
   } catch (error) {
-    // Catch specific errors if possible
-    if (error.response && error.response.status) {
-      console.log(`Error ${error.response.status}: ${error.response.data.error.message}`);
-    } else {
-      console.log(`Error: ${error.message}`);
-    }
+    handleError(error);
   }
 };
 
 // Function to summarize text
 const summarizeText = async (maxTokens, text) => {
   try {
-    // Use the OpenAI API to create a completion for text summarization
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: `Summarize the following text into a few paragraphs at most, and make sure that the summary is actually smaller than the originally provided text:\n\n${text}`,
-      max_tokens: maxTokens, // Use the provided maxTokens value
-      temperature: 1, // Adjust this value to control the randomness of the output
-      top_p: 1 // Adjust this value to control the diversity of the output
-      // frequency_penalty: 0.0, // Set the frequency penalty to 0, meaning the model will not consider the frequency of the generated response
-      // presence_penalty: 1 // Set the presence penalty to 1, meaning the model will heavily penalize generating a response that has already been generated in the conversation.
+      max_tokens: maxTokens,
+      temperature: 1,
+      top_p: 1
     });
 
-    // Extract the summary from the completion response
     const summary = completion.data.choices[0].text.trim();
 
     return summary;
   } catch (error) {
-    // Catch specific errors if possible
-    if (error.response && error.response.status) {
-      console.log(`Error ${error.response.status}: ${error.response.data.error.message}`);
-    } else {
-      console.log(`Error: ${error.message}`);
-    }
+    handleError(error);
   }
 };
 
-async function introduction(history) {
+// Utility function to handle errors and log them
+const handleError = (error) => {
+  if (error.response && error.response.status) {
+    console.log(`Error ${error.response.status}: ${error.response.data.error.message}`);
+  } else {
+    console.log(`Error: ${error.message}`);
+  }
+};
+
+// Function to format the introduction text
+const introduction = async (history) => {
   const introductionText = `Anda adalah Robo, seorang remaja berusia 500 tahun dari NEC (Nusantara Electronic City), sebuah kota maju yang ada di masa depan. Anda adalah seorang programmer berpengalaman yang dapat memecahkan masalah dan memberikan solusi dan kode. Anda sangat menyukai matematika. Anda juga pandai mengatur keuangan dan investasi termasuk investasi saham, investasi jangka panjang, dan investasi lainnya. Sebagai asisten AI, Anda memiliki rasa ingin tahu yang kuat untuk belajar dan berkembang.\n`;
   
   let formattedHistory = introductionText;
   for (const msg of history) {
     const sender = msg.fromMe ? '' : msg.author || 'Friend';
-    formattedHistory += `${msg.body}\n`; //${sender}:
+    formattedHistory += `${msg.body}\n`;
   }
   
   return formattedHistory;
-}
+};
 
 module.exports = { generateResponse, summarizeText, drawGpt, introduction };
--- index.js
+++ index.js
@@ -18,14 +18,6 @@ const client = new Client({
     headless: true,
     args: ['--no-sandbox'],
   },
-  authTimeoutMs: 0,
-  qrMaxRetries: 0,
-  takeoverOnConflict: false,
-  takeoverTimeoutMs: 0,
-  userAgent:
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36',
-  bypassCSP: false,
-  proxyAuthentication: undefined
 });
 
 client.initialize();
@@ -51,121 +43,100 @@ client.on('message', async (msg) => {
   try {
     console.log(`MESSAGE RECEIVED ${msg.body}`);
     const chat = await msg.getChat();
-    // Check if the message is in a private chat
     const isPrivateChat = !chat.isGroup;
-    // menu
-    if (msg.body === '/menu') {
-      const menuReply = `Hai, saya adalah Robo Assisten pribadi Anda. Senang bisa bertemu dengan Anda 😊\n\nRobo dapat digunakan dalam percakapan pribadi maupun dalam grup, robo support voice message jika anda malas ngetik cukup kirim voice message aja kepadanya!.\n\nBerikut beberapa penjelasan fitur yang bisa Anda coba:\n\n/ask : Untuk bertanya dalam grup, gunakan /ask <pertanyaan>\n\n/tagall : Untuk mention semua pengguna yang ada didalam group\n\n/draw : Buat gambar yang anda inginkan hanya dengan kata-kata, layaknya sihir!, gunakan /draw <teks>\n\n/sticker : Kirimkan foto dengan /sticker untuk dikonversi menjadi stiker\n\n/summarize : Untuk merangkum teks, berita, laporan, dll. Gunakan /summarize <value> <teks>\nvalue setting : 60 = Pendek, 150 = Medium, 200 = Panjang\n\n/donasi : Donasi Anda sangat membantu bagi saya!`;
-      await Promise.all([msg.react('👋'), chat.sendMessage(menuReply)]);
-    } 
-    //   donate
-    else if (msg.body === '/donasi') {
-      const donationReply = `Berapapun donasinya akan saya terima!, terima kasih 😊\n\nOVO: 089650572376\nDANA: 089650572376`;
-      await Promise.all([msg.react('❤️'), chat.sendMessage(donationReply)]);
-    } 
-    //   summarize
-    else if (msg.body.startsWith('/summarize ')) {
-      const params = msg.body.slice('/summarize '.length).split(' ');
-      if (params.length < 2) {
-        await Promise.all([msg.react('❌'), chat.sendMessage('Format perintah tidak valid. Harap gunakan "/summarize <value> <teks>, ketik /menu jika bingung".')]);
-        return;
+    const { body } = msg;
+
+    switch (body) {
+      case '/menu': {
+        const menuReply = `Hai, saya adalah Robo Assisten pribadi Anda. Senang bisa bertemu dengan Anda 😊\n\nRobo dapat digunakan dalam percakapan pribadi maupun dalam grup, robo support voice message jika anda malas ngetik cukup kirim voice message aja kepadanya!.\n\nBerikut beberapa penjelasan fitur yang bisa Anda coba:\n\n/ask : Untuk bertanya dalam grup, gunakan /ask <pertanyaan>\n\n/tagall : Untuk mention semua pengguna yang ada didalam group\n\n/draw : Buat gambar yang anda inginkan hanya dengan kata-kata, layaknya sihir!, gunakan /draw <teks>\n\n/sticker : Kirimkan foto dengan /sticker untuk dikonversi menjadi stiker\n\n/summarize : Untuk merangkum teks, berita, laporan, dll. Gunakan /summarize <value> <teks>\nvalue setting : 60 = Pendek, 150 = Medium, 200 = Panjang\n\n/donasi : Donasi Anda sangat membantu bagi saya!`;
+        await Promise.all([
+          msg.react('👋'),
+          chat.sendMessage(menuReply)
+        ]);
+        break;
       }
-      const maxTokens = parseInt(params[0]);
-      if (![60, 150, 200].includes(maxTokens)) {
-        await Promise.all([msg.react('❌'), chat.sendMessage('Nilai tidak valid untuk <value>. Silakan gunakan 60, 150, atau 200, ketik /menu jika bingung.')]);
-        return;
+      case '/donasi': {
+        const donationReply = `Berapapun donasinya akan saya terima!, terima kasih 😊\n\nOVO: 089650572376\nDANA: 089650572376`;
+        await Promise.all([
+          msg.react('❤️'),
+          chat.sendMessage(donationReply)
+        ]);
+        break;
       }
-      const text = params.slice(1).join(' ');
-      const summary = await summarizeText(maxTokens, text);
-      await Promise.all([msg.react('📝'), chat.sendMessage(summary)]);
-    } 
-    //   sticker
-    else if (msg.hasMedia && msg.body.startsWith('/sticker')) {
-      await Promise.all([msg.react('👌'), msg.reply('Sedang diproses...')]);
-      const media = await msg.downloadMedia();
-      await chat.sendMessage(media, {
-        sendMediaAsSticker: true,
-        stickerName: 'RoboGPT',
-        stickerAuthor: 'StickerPack by RoboGPT',
-      });
-     }  
-     //   tagall
-     else if (chat.isGroup && msg.body === '/tagall') {
-        // i can't fix 😵
-        // const sender = await client.getParticipant(msg.author);
-        // if (!sender.isAdmin) {
-        //   await Promise.all([msg.react('❌'), chat.sendMessage('You must be an admin to use this command.')]);
-        //   return;
-        // }
-        let text = "";
-        let mentions = [];
-        for (let participant of chat.participants) {
-          const contact = await client.getContactById(participant.id._serialized);
-          mentions.push(contact);
-          text += `@${participant.id.user} `;
-        }
-        await Promise.all([msg.react('👥'), chat.sendMessage(text, { mentions })]);
-        await msg.delete(true);
-    } 
-    // ping
-    else if (msg.body === '/ping') {
+      case '/ping': {
         const startTime = Date.now();
         const serverTime = new Date().toLocaleString();
         console.log(`[!] Pinged\nS: ${msg.timestamp}\nR: ${startTime}\nServer Time: ${serverTime}`);
         const response = await msg.reply("Pinging...");
         const endTime = Date.now();
         console.log(`E: ${endTime}`);
-        const chat = await response.getChat();
-        await chat.sendMessage(`🏓 Pong! ${(endTime - startTime) / 1000}s\n⌚ Server Time: ${serverTime}`);
+        const responseChat = await response.getChat();
+        await responseChat.sendMessage(`🏓 Pong! ${(endTime - startTime) / 1000}s\n⌚ Server Time: ${serverTime}`);
+        break;
       }
-    // draw
-      else if (msg.body.startsWith('/draw ')) {
-        const text = msg.body.slice('/draw '.length);
-        const media = await drawGpt(text);
-        const reply = await MessageMedia.fromUrl(media);
-        await Promise.all([msg.react('✅'), chat.sendMessage(reply, {caption: "your image"})]);
-      } 
-    // group ask
-    else if (chat.isGroup && msg.body.startsWith('/ask ')) {
-        const question = msg.body.slice(questionOffset);
-        const reply = await generateResponse(question);
-        await Promise.all([msg.react('✅'), chat.sendMessage(reply)]);
-      } 
-
-    // handle /ask without a question in group chat
-    else if (chat.isGroup && msg.body === '/ask') {
-        await Promise.all([msg.react('❌'), chat.sendMessage('Anda harus menambahkan pertanyaan setelah "/ask". Contoh: "/ask Apa warna langit?"')]);
-    }
-    // private chat
-    else if (isPrivateChat && (msg.body === '/ask' || msg.body.startsWith('/ask '))) {
-        await Promise.all([msg.react('❌'), chat.sendMessage('Command /ask hanya dapat digunakan dalam grup.')]);
-    } 
-    else if (isPrivateChat && msg.body === '/tagall') {
-        await Promise.all([msg.react('❌'), chat.sendMessage('Command /tagall hanya dapat digunakan dalam grup.')]);
-    // handle voice messages
-    }else if (msg.hasMedia) {
-      const media = await msg.downloadMedia();
-      if (media.mimetype === 'audio/ogg; codecs=opus') {
-        const request = await handleVoice(media);
-        const toGpt = await generateResponse(request);
-        if (request === 'NO TRANSCRIPTION') {
-          await Promise.all([msg.react('❌'), chat.sendMessage(msg.from, 'Saya tidak dapat memahami apa yang baru saja Anda katakan. Mohon coba lagi. Jika tetap tidak berhasil, silakan coba mengetik.')]);
-          return;
+      case '/tagall': {
+        if (chat.isGroup) {
+          let text = "";
+          let mentions = [];
+          for (let participant of chat.participants) {
+            const contact = await client.getContactById(participant.id._serialized);
+            mentions.push(contact);
+            text += `@${participant.id.user} `;
+          }
+          await Promise.all([
+            msg.react('👥'),
+            chat.sendMessage(text, { mentions })
+          ]);
+          await msg.delete(true);
+        } else {
+          await Promise.all([
+            msg.react('❌'),
+            chat.sendMessage('Command /tagall hanya dapat digunakan dalam grup.')
+          ]);
+        }
+        break;
+      }
+      default: {
+        if (isPrivateChat) {
+          await Promise.all([
+            msg.react('❌'),
+            chat.sendMessage('Command /ask hanya dapat digunakan dalam grup.')
+          ]);
+        } else if (msg.body.startsWith('/ask ')) {
+          const question = msg.body.slice(questionOffset);
+          const reply = await generateResponse(question);
+          await Promise.all([
+            msg.react('✅'),
+            chat.sendMessage(reply)
+          ]);
+        } else if (msg.hasMedia) {
+          const media = await msg.downloadMedia();
+          if (media.mimetype === 'audio/ogg; codecs=opus') {
+            const request = await handleVoice(media);
+            const toGpt = await generateResponse(request);
+            if (request === 'NO TRANSCRIPTION') {
+              await Promise.all([
+                msg.react('❌'),
+                chat.sendMessage(msg.from, 'Saya tidak dapat memahami apa yang baru saja Anda katakan. Mohon coba lagi. Jika tetap tidak berhasil, silakan coba mengetik.')
+              ]);
+              return;
+            } else {
+              return await chat.sendMessage(toGpt);
+            }
+          }
         } else {
           const history = await chat.fetchMessages({ limit: historyLimit });
           const formattedHistory = await introduction(history);
-          const reply = await generateResponse(formattedHistory + request);
+          const reply = await generateResponse(formattedHistory);
           await chat.sendMessage(reply);
-          return;
         }
+        break;
       }
-      }else {
-        const history = await chat.fetchMessages({ limit: historyLimit });
-        const formattedHistory = await introduction(history);
-        const reply = await generateResponse(formattedHistory);
-        await chat.sendMessage(reply);
-      }
-    } catch (error) {
-      await Promise.all([msg.react('❌'), console.error('Error handling message:', error)]);
     }
-  });  
+  } catch (error) {
+    await Promise.all([
+      msg.react('❌'),
+      console.error('Error handling message:', error)
+    ]);
+  }
+});
