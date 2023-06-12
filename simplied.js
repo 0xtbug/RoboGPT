c43192e45c4b2de0d659770fe97161e135a714d9
0xtbug <tubagus.nabil19@gmail.com>
2023-06-12 09:34:06 +0700

    test

--- simplied.js
+++ simplied.js
@@ -10,23 +10,24 @@ const historyLimit = parseInt(process.env.HISTORY_LIMIT);
 const questionOffset = parseInt(process.env.QUESTION_OFFSET);
 
 const client = new Client({
-  authStrategy: new LocalAuth({
+  auth: new LocalAuth({
     clientId: 'client',
-    dataPath: './sessions',
+    clientOptions: {
+      session: './sessions/session',
+      puppeteer: {
+        headless: true,
+        args: ['--no-sandbox'],
+      },
+    },
   }),
-  puppeteer: {
-    headless: true,
-    args: ['--no-sandbox'],
-  },
-  authTimeoutMs: 0,
-  qrMaxRetries: 0,
+  ffmpegPath: ffmpegPath,
+  puppeteer: { executablePath: '/usr/bin/chromium-browser' },
+  sessionPath: './sessions/session',
+  qrTimeoutMs: 0,
   takeoverOnConflict: false,
   takeoverTimeoutMs: 0,
   userAgent:
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36',
-  ffmpegPath: 'ffmpeg',
-  bypassCSP: false,
-  proxyAuthentication: undefined,
 });
 
 client.initialize();
@@ -37,7 +38,7 @@ client.on('qr', (qr) => {
 });
 
 client.on('authenticated', () => {
-  console.log(`✓ Authenticated!`);
+  console.log('✓ Authenticated!');
 });
 
 client.on('auth_failure', (msg) => {
@@ -52,17 +53,19 @@ client.on('message', async (msg) => {
   try {
     console.log(`MESSAGE RECEIVED ${msg.body}`);
     const chat = await msg.getChat();
-
+    // menu
     if (msg.body === '/menu') {
       const menuReply = `Hai, saya adalah Robo Assisten pribadi Anda. Senang bisa bertemu dengan Anda 😊\n\nRobo dapat digunakan dalam percakapan pribadi maupun dalam grup.\n\nBerikut beberapa penjelasan fitur yang bisa Anda coba:\n\n/ask : Untuk bertanya dalam grup, gunakan /ask <pertanyaan>\n\n/tagall : Untuk mention semua pengguna yang ada didalam group\n\n/sticker : Kirimkan foto dengan /sticker untuk dikonversi menjadi stiker\n\n/summarize : Untuk merangkum teks, berita, laporan, dll. Gunakan /summarize <value> <teks>\nvalue setting : 60 = Pendek, 150 = Medium, 200 = Panjang\n\n/donasi : Donasi Anda sangat membantu bagi saya!`;
       await Promise.all([msg.react('👋'), chat.sendMessage(menuReply)]);
-    } else if (msg.body === '/donasi') {
+    } //   donate
+    else if (msg.body === '/donasi') {
       const donationReply = `Berapapun donasinya akan saya terima!, terima kasih 😊\n\nOVO: 089650572376\nDANA: 089650572376`;
       await Promise.all([msg.react('❤️'), chat.sendMessage(donationReply)]);
-    } else if (msg.body.startsWith('/summarize ')) {
+    } //   summarize
+    else if (msg.body.startsWith('/summarize ')) {
       const params = msg.body.slice('/summarize '.length).split(' ');
       if (params.length < 2) {
-        await Promise.all([msg.react('❌'), chat.sendMessage('Format perintah tidak valid. Harap gunakan "/summarize <nilai> <teks>, ketik /menu jika bingung".')]);
+        await Promise.all([msg.react('❌'), chat.sendMessage('Format perintah tidak valid. Harap gunakan "/summarize <value> <teks>, ketik /menu jika bingung".')]);
         return;
       }
       const maxTokens = parseInt(params[0]);
@@ -73,7 +76,8 @@ client.on('message', async (msg) => {
       const text = params.slice(1).join(' ');
       const summary = await summarizeText(maxTokens, text);
       await Promise.all([msg.react('📝'), chat.sendMessage(summary)]);
-    } else if (msg.hasMedia && msg.body.startsWith('/sticker')) {
+    } //   sticker
+    else if (msg.hasMedia && msg.body.startsWith('/sticker')) {
       await Promise.all([msg.react('👌'), msg.reply('Foto sedang diproses...')]);
       const media = await msg.downloadMedia();
       await chat.sendMessage(media, {
@@ -81,26 +85,36 @@ client.on('message', async (msg) => {
         stickerName: 'RoboGPT',
         stickerAuthor: 'StickerPack by RoboGPT',
       });
-     } else if (chat.isGroup && msg.body === '/tagall') {
-        // i can't fix 😵
-        // const sender = await client.getParticipant(msg.author);
-        // if (!sender.isAdmin) {
-        //   await Promise.all([msg.react('❌'), chat.sendMessage('You must be an admin to use this command.')]);
-        //   return;
-        // }
-        let text = "";
-        let mentions = [];
-        for (let participant of chat.participants) {
-          const contact = await client.getContactById(participant.id._serialized);
-          mentions.push(contact);
-          text += `@${participant.id.user} `;
-        }
-        await  Promise.all([msg.react('👥'), chat.sendMessage(text, { mentions })]);
-    } else if (chat.isGroup && msg.body.startsWith('/ask ')) {
+    }  //   tagall
+    else if (chat.isGroup && msg.body === '/tagall') {
+      const sender = await chat.getContactById(msg.author);
+      if (!sender.isAdmin) {
+        await Promise.all([msg.react('❌'), chat.sendMessage('You must be an admin to use this command.')]);
+        return;
+      }
+      let text = "";
+      let mentions = [];
+      for (let participant of chat.participants) {
+        const contact = await client.getContactById(participant.id._serialized);
+        mentions.push(contact);
+        text += `@${participant.id.user} `;
+      }
+      await Promise.all([msg.react('👥'), chat.sendMessage(text, { mentions })]);
+    } // ping
+    else if (msg.body === '/ping') {
+      const timestamp = Math.floor(Date.now() / 1000);
+      console.log(`[!] Pinged\nS: ${msg.timestamp}\nR: ${timestamp}`);
+      const response = await msg.reply("Pinging...");
+      console.log(`E: ${response.timestamp}`);
+      const chat = await response.getChat();
+      await chat.sendMessage(`🏓 Pong! ${(response.timestamp - msg.timestamp)}s`);
+    } // group
+    else if (chat.isGroup && msg.body.startsWith('/ask ')) {
       const question = msg.body.slice(questionOffset);
       const reply = await generateResponse(question);
       await Promise.all([msg.react('✅'), chat.sendMessage(reply)]);
-    } else if (!chat.isGroup && !msg.body.startsWith('/ask ')) {
+    } // private
+    else if (!chat.isGroup && !msg.body.startsWith('/ask ')) {
       const history = await chat.fetchMessages({ limit: historyLimit });
       const introduction = `You are Robo, a 500-year-old teenager from NEC (Nusantara Electronic City), an advanced city that exists in the future. You are an experienced programmer who can solve problems and provide solutions and codes. You are very fond of math. You are also good at managing finances and investments including stock investments, long-term investments, and other investments. As an AI assistant, you have a strong curiosity to learn and grow.\n`;
       let formattedHistory = introduction;
